69d1e808334b7b3c2a496711586a1019d47e0787
Adam Durrett <56136019+Partysquid@users.noreply.github.com>
2021-02-21 08:23:14 -0500

    Update scrapper.py

--- Reddit_scraper/scrapper.py
+++ Reddit_scraper/scrapper.py
@@ -1,15 +1,5 @@
 from consume_reddit_sql import *
-# connection = pymysql.connect(host='localhost',
-# 	                             user='root',
-# 	                             password='dongs420',
-# 	                             db='LeagueDB',
-# 	                             charset='utf8mb4',
-# 	                             cursorclass=pymysql.cursors.DictCursor)
 
-
-#old code for database auto upload
-#connection = pymysql.connect(host='lolsql.stat.ncsu.edu', user='LOLwrite',password='LaberLabsLOLwrite',db='lol_test',charset='utf8mb4')
-#x=consume_reddit(connection)
 
 reddit_data_dict = { "title":[],
                 "score":[],
